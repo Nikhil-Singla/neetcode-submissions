a9ba9ec8f5399ef37cc35d32ca297d2637440a7d
Nikhil Singla <nsingla3.14@gmail.com>
2026-06-13 17:09:20 -0500

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return "NULL"
+        chars = set(''.join(strs))
+        for i in range(1, 1000):
+            if chr(i) not in chars:
+                delimiter = chr(i)
+        
+        ans = delimiter + delimiter.join(strs)
+        return ans
+
+    def decode(self, s: str) -> List[str]:
+        if s == "NULL":
+            return []
+        delimiter = s[0]
+        ret = s.split(delimiter)
+        del ret[0]
+        return ret 
